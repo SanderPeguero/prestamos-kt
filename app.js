0e911874e4c8a1d2a9c5b8774a53cd3449650a8e
Sander Peguero <sanderpeguero.21@gmail.com>
2022-09-30 22:13:29 -0400

    Router

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/', ArticulosRouter)
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the QuantumSwap API!')
-})
-
 app.listen(process.env.PORT || 4000, () => {
     console.log('Server Started!')
 })
